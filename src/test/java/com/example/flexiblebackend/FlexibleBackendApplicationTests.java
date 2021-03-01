529c185ff0d4e66dab7bd7e19eb70e9194e9b65d
Erncheng <ernchenghoo2@gmail.com>
2021-03-01 11:59:12 +0800

    initial commit

---
+++ src/test/java/com/example/flexiblebackend/FlexibleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flexiblebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexibleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
